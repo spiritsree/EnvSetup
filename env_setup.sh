3f0fe2951c1fc5ab096f1f0c93596f92f7c302ae
Sreejith G <spirit.sree@gmail.com>
2019-12-17 17:06:41 +1100

    adding new pkg

--- env_setup.sh
+++ env_setup.sh
@@ -5,7 +5,7 @@
 ########################################################################################################
 
 # Global Vars
-PKG_INSTALLS_COMMON='bash-completion zsh zsh-completions watch tree git tig screen tmux ruby jq yq python3 pip3 htop yamllint jsonlint shellcheck jid colordiff go stubby knot'
+PKG_INSTALLS_COMMON='bash-completion zsh zsh-completions watch tree git tig screen tmux ruby jq yq python3 pip3 htop yamllint jsonlint shellcheck jid colordiff go stubby knot hub'
 PKG_INSTALLS_WORK='docker docker-compose kubernetes-helm kubernetes-cli kops vagrant virtualbox terraform'
 PKG_INSTALLS_PERSONAL='google-chrome utorrent atom sublime-text vlc firefox 4k-video-downloader 4k-stogram 4k-youtube-to-mp3 4k-video-to-mp3 dash'
 PIP_INSTALLS='virtualenv awscli boto3'
@@ -605,4 +605,5 @@ main "$@"
 # zsh: https://ohmyz.sh/
 # jid: https://github.com/simeji/jid
 # Bash Cheatsheet: https://devhints.io/bash
+# hub: https://github.com/github/hub
 
