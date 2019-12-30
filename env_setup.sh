31601154b2b0dc03dcc7928d86d30604024f2ff1
Sreejith G <spirit.sree@gmail.com>
2019-12-30 15:01:33 +1100

    add pylintrc

--- env_setup.sh
+++ env_setup.sh
@@ -9,7 +9,7 @@ PKG_INSTALLS_COMMON='bash-completion zsh zsh-completions watch tree git tig scre
 PKG_INSTALLS_WORK='docker docker-compose kubernetes-helm kubernetes-cli kops vagrant virtualbox terraform'
 PKG_INSTALLS_PERSONAL='google-chrome utorrent atom sublime-text vlc firefox 4k-video-downloader 4k-stogram 4k-youtube-to-mp3 4k-video-to-mp3 dash'
 PIP_INSTALLS='virtualenv awscli boto3 pylint'
-APP_PROFILES='.vimrc .gvimrc .tmux.conf .tmux-osx.conf .gemrc .tigrc .screenrc .irbrc .inputrc .gitconfig .gitignore .yamllint'
+APP_PROFILES='.vimrc .gvimrc .tmux.conf .tmux-osx.conf .gemrc .tigrc .screenrc .irbrc .inputrc .gitconfig .gitignore .yamllint .pylintrc'
 
 PROFILES_DIR="./profiles"
 THEMES_DIR="./themes"
